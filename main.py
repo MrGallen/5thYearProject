1d2c18c0ba530d12320999718e1110ad59aa8b9e
MrGallen <eoinfarrah@hotmail.com>
2020-05-03 13:31:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#You need to install in command line both imports below
+#pip install pyrebase4
+import pyrebase
+#pip install matplotlib
+from matplotlib import pyplot as plt
+
+#config = {
+#    "apiKey": "API Key From Firebase CDN",
+#    "authDomain": "AuthDomain From Firebase CDN",
+#    "databaseURL": "Database url From Firebase CDN",
+#    "storageBucket": "storageBucket from Firebase CDN"
+#    }
+
+#firebase = pyrebase.initialize_app(config)
+
+# Get a reference to the database service
+#db = firebase.database()
+
+#Enter your Python Code For Program - Dont forget to pip install the imports you need
+#Some imports like time and random dont need installed
